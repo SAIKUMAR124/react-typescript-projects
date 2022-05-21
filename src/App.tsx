a5a20339c902fb3115882efd8cb5a79ba54ff715
dev global <pavankumar.s12459@gmail.com>
2022-05-21 16:32:42 +0530

    Formik Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import FormikForm from "./Pages/FormikForm";
 import Home from "./Pages/Home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/form" element={<FormikForm />} />
       </Routes>
     </Router>
   );
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <h3>
+        <Link to="/form">Formik Form</Link>
+      </h3>
     </div>
   );
 };
